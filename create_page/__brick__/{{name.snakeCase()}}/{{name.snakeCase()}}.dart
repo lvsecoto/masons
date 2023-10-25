c0a1fdfad058a3a740aa8596f2d87d24cff04814
lvsecoto <lvsecoto@gmail.com>
2023-10-25 09:41:49 +0800

    Update {{name.snakeCase()}}.dart

--- create_page/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
+++ create_page/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
@@ -1,11 +1,9 @@
-import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 
 import 'widget/content_widget.dart';
 
-@RoutePage()
-class {{name.pascalCase()}}PageWidget extends StatelessWidget {
-  const {{name.pascalCase()}}PageWidget({super.key});
+class {{name.pascalCase()}}Screen extends StatelessWidget {
+  const {{name.pascalCase()}}Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
